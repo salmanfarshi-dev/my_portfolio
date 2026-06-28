0167dd9455bfa14757ee3f0a7b51fc8d014ce32e
Salman Farshi <sfarshi222@gmail.com>
2026-06-29 00:20:54 +0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-text-primary bg-bg-primary py-6 text-center text-4xl font-bold'>
+      Salman farshi
+    </div>
+  )
+}
+
+export default App
