8b8443a94fc158bdb599e9231672a2c543f0ff52
Salman Farshi <sfarshi222@gmail.com>
2026-07-01 14:04:57 +0600

    feat: add card add

--- src/Component/Tools.jsx
+++ src/Component/Tools.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tools({className, tool}) {
   return (
-    <div className={`bg-text-secondary/40 rounded-[14px]  py-2 px-5 border border-border text-text-primary font-semibold ${className}`}>{tool}</div>
+    <div className={`bg-text-secondary/10 rounded-[14px]  py-2 px-5 border border-border text-text-primary font-semibold ${className}`}>{tool}</div>
   )
 }
 
--- src/Layout/ServiceCard.jsx
+++ src/Layout/ServiceCard.jsx
@@ -5,18 +5,23 @@ import CardTittle from "../Component/CardTittle";
 import CardPera from "../Component/CardPera";
 import Tools from "../Component/Tools";
 import { FaMobileAlt } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
+import { MdColorLens } from "react-icons/md";
 
 function ServiceCard() {
   return (
     <section className="mt-15">
       <div className="grid grid-cols-12 gap-6">
-        <div className="col-span-8">
+        <div className="col-span-12 md:col-span-8">
           <div className="card">
             <Icon
               icon={<FaReact className="text-2xl text-text-primary" />}
               className="bg-primary/30"
             />
-            <CardTittle head="React Frontend Development" className="my-3" />
+            <CardTittle
+              head="React Frontend Development"
+              className="mt-2 mb-3"
+            />
             <CardPera pera="I build component-based UIs that are reusable, clean, and responsive. I create scalable frontends using hooks, state management, and modern React patterns." />
 
             <div className="mt-7 flex gap-x-5">
@@ -53,22 +58,129 @@ function ServiceCard() {
             </div>
           </div>
         </div>
-        <div className="col-span-4">
+        <div className="col-span-12 md:col-span-4">
           <div className="card">
-            <Icon icon={<FaMobileAlt className="text-2xl text-text-primary"/>} className="bg-accent/20 border-accent/30 border"/>
-            <CardTittle head="Responsive UI" className="my-3"/>
-            <CardPera pera="Pixel-perfect UI across all screen sizes, built with a mobile-first approach."/>
+            <Icon
+              icon={<FaMobileAlt className="text-2xl text-text-primary" />}
+              className="bg-accent/20 border-accent/30 border"
+            />
+            <CardTittle head="Responsive UI" className="mt-2 mb-3" />
+            <CardPera pera="Pixel-perfect UI across all screen sizes, built with a mobile-first approach." />
 
-             <div className="mt-6 flex items-center flex-wrap gap-4">
-             
-              <Tools tool="CSS3" className="bg-accent/30! border border-border-hover/70"/>
-              <Tools tool="Grid" className="bg-accent/30! border border-border-hover/70"/>
+            <div className="mt-6 flex items-center flex-wrap gap-4">
+              <Tools
+                tool="CSS3"
+                className="bg-accent/10! border border-border-hover/50"
+              />
+              <Tools
+                tool="Grid"
+                className="bg-accent/10! border border-border-hover/50"
+              />
               <Tools tool="Flexbox" />
               <Tools tool="Media Query" />
-             
             </div>
+          </div>
+        </div>
+        <div className="col-span-12 md:col-span-4">
+          <div className="card">
+            <CardTittle head="My process" />
 
+            <div className="mt-6 flex flex-col gap-y-4">
+              <div className="flex gap-x-3 border-b pb-6 border-border">
+                <Icon icon="01" className="myprocess" />
 
+                <div className="flex flex-col gap-y-1">
+                  <h5 className="text-xl text-text-primary font-medium ">
+                    Understand
+                  </h5>
+                  <p className="text-sm text-text-secondary/60">
+                    I understand your requirements.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-3 border-b pb-6 border-border">
+                <Icon icon="02" className="myprocess" />
+
+                <div className="flex flex-col gap-y-1">
+                  <h5 className="text-xl text-text-primary font-medium">
+                    Design
+                  </h5>
+                  <p className="text-sm text-text-secondary/60">
+                    I plan the UI..
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-3 border-b pb-6 border-border">
+                <Icon icon="03" className="myprocess" />
+
+                <div className="flex flex-col gap-y-1">
+                  <h5 className="text-xl text-text-primary font-medium">
+                    Build
+                  </h5>
+                  <p className="text-sm text-text-secondary/60">
+                    I code and deliver the final product.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-3 border-b pb-6 border-border">
+                <Icon icon="04" className="myprocess" />
+
+                <div className="flex flex-col gap-y-1">
+                  <h5 className="text-xl text-text-primary font-medium ">
+                    Revise
+                  </h5>
+                  <p className="text-sm text-text-secondary/60">
+                    I refine and improve based on feedback..
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-12 md:col-span-4">
+          <div className="card">
+            <Icon
+              icon={<FiFigma className="text-primary text-2xl" />}
+              className="bg-primary/10 border border-primary/20"
+            />
+
+            <CardTittle head="Figma to Code" className="mt-2 mb-3" />
+            <CardPera pera="I convert designs into responsive, pixel-perfect user interfaces that match the original concept." />
+
+            <div className="mt-6 flex items-center flex-wrap gap-4">
+              <Tools
+                tool="Figma"
+                className="bg-pink-200/10! border border-border-hover/50"
+              />
+              <Tools
+                tool="HTML"
+                className="bg-pink-200/10! border border-border-hover/50"
+              />
+              <Tools tool="CSS" />
+              <Tools tool="Responsive Design" />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-12 md:col-span-4">
+          <div className="card">
+            <Icon icon={<MdColorLens className="text-white text-2xl"/>} className="bg-accent/20 border border-accent/40"/>
+            <CardTittle head="Landing Pages" className="mt-2 mb-3"/>
+            <CardPera pera="Any type of page — personal portfolio, startup landing page, or product showcase. Fast, animated, and SEO-friendly."/>
+
+             <div className="mt-6 flex items-center flex-wrap gap-4">
+              <Tools
+                tool="HTML/CSS"
+                className="bg-purple-400/10! border border-border-hover/20"
+              />
+              <Tools
+                tool="JavaScript"
+                className="bg-purple-400/10! border border-border-hover/20"
+              />
+              <Tools tool="React" />
+              <Tools tool="Animation" />
+            </div>
           </div>
         </div>
       </div>
