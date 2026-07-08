000ba1f3733fa23ceb35d410051c82a4d97c6035
Salman Farshi <sfarshi222@gmail.com>
2026-07-09 00:02:23 +0600

    fead: add contact section fully designed

--- src/Component/ContactList.jsx
+++ src/Component/ContactList.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-
+import { TfiEmail } from "react-icons/tfi";
+import { IoTime } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 
 
@@ -16,33 +23,87 @@ function ContactList() {
     <p className='mt-2 text-[16px] md:text-[18px] md:w-[70%] w-full flex-wrap text-text-secondary font-normal'>Have a project in mind or looking for a Frontend Developer? Feel free to reach out. I'm always open to discussing new opportunities, freelance projects, and creative collaborations.</p>
     <div className="flex flex-col mt-3 gap-y-2">
       <div className="flex items-center gap-x-3 bg-bg-secondary py-2 px-4 w-[40%] rounded-[10px] border border-border">
-        <div className="w-10 h-10 bg-primary flex justify-center items-center rounded-[10px]"></div>
+        <div className="w-10 h-10 bg-primary flex justify-center items-center rounded-[10px]">
+          <TfiEmail />
+
+        </div>
         <div className="flex flex-col">
           <h4 className='font-medium text-text-primary'>Email</h4>
           <p className='font-light text-text-secondary'>salmanfarshidevel@gmail.com</p>
         </div>
       </div>
       <div className="flex items-center gap-x-3 bg-bg-secondary py-2 px-4 w-[40%] rounded-[10px] border border-border">
-        <div className="w-10 h-10 bg-primary flex justify-center items-center rounded-[10px]"></div>
+        <div className="w-10 h-10 bg-blue-600 flex justify-center items-center rounded-[10px]">
+          <FaPhoneVolume />
+
+        </div>
         <div className="flex flex-col">
           <h4 className='font-medium text-text-primary'>Phone</h4>
           <p className='font-light text-text-secondary'>+8801324385585</p>
         </div>
       </div>
       <div className="flex items-center gap-x-3 bg-bg-secondary py-2 px-4 w-[40%] rounded-[10px] border border-border">
-        <div className="w-10 h-10 bg-primary flex justify-center items-center rounded-[10px]"></div>
+        <div className="w-10 h-10 bg-green-400 flex justify-center items-center rounded-[10px]">
+          <FaLocationDot />
+
+
+        </div>
         <div className="flex flex-col">
           <h4 className='font-medium text-text-primary'>Location</h4>
           <p className='font-light text-text-secondary'>Dhaka,Bangladesh</p>
         </div>
       </div>
       <div className="flex items-center gap-x-3 bg-bg-secondary py-2 px-4 w-[40%] rounded-[10px] border border-border">
-        <div className="w-10 h-10 bg-primary flex justify-center items-center rounded-[10px]"></div>
+        <div className="w-10 h-10 bg-orange-400 flex justify-center items-center rounded-[10px]">
+          <IoTime />
+
+        </div>
         <div className="flex flex-col">
           <h4 className='font-medium text-text-primary'>Respons Time</h4>
           <p className='font-light text-text-secondary'>Within 24 Hours</p>
         </div>
       </div>
+    </div>
+
+    <div className="mt-3">
+      <p className='text-text-primary font-normal '>Find me on</p>
+       <div className=" flex pt-2 gap-x-4 items-center">
+  <a
+    href="https://www.facebook.com/share/1DppoBYrNY/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="iconlist"
+  >
+    <FaFacebookF />
+  </a>
+
+  <a
+    href="https://x.com/salmanfardev"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="iconlist"
+  >
+    <FaTwitter />
+  </a>
+
+  <a
+    href="https://www.linkedin.com/in/salmanfarshi-dev/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="iconlist"
+  >
+    <FaLinkedinIn />
+  </a>
+
+  <a
+    href="https://github.com/salmanfarshi-dev"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="iconlist"
+  >
+    <FaGithub />
+  </a>
+</div>
     </div>
 
    
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import { FaFacebookF } from "react-icons/fa6";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+
 
 
 function Footer() {
@@ -12,43 +9,7 @@ function Footer() {
      <div className="flex justify-between items-center flex-col md:flex-row gap-y-5 md:gap-y-0 ">
         <p className='text-text-secondary/60 text-sm md:text-[16px]'>Designed & Developed by Salman Farshi · 2026</p>
 
- <div className=" flex gap-x-4 items-center">
-  <a
-    href="https://www.facebook.com/share/1DppoBYrNY/"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="iconlist"
-  >
-    <FaFacebookF />
-  </a>
 
-  <a
-    href="https://x.com/salmanfardev"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="iconlist"
-  >
-    <FaTwitter />
-  </a>
-
-  <a
-    href="https://www.linkedin.com/in/salmanfarshi-dev/"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="iconlist"
-  >
-    <FaLinkedinIn />
-  </a>
-
-  <a
-    href="https://github.com/salmanfarshi-dev"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="iconlist"
-  >
-    <FaGithub />
-  </a>
-</div>
     </div>
    </div>
 
