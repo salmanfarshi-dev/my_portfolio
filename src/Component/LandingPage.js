7fa26d8e25f24273088169e045440adfe0f747e6
Salman Farshi <sfarshi222@gmail.com>
2026-07-17 14:59:30 +0600

    solve image show

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -3,7 +3,7 @@
 const landingapi =[
     {
       id:1,
-      image: '/public/finsweet.png',
+      image: '/finsweet.png',
       link:"https://salmanfarshi-dev.github.io/Finsweet/",
       tittle:"Finsweet",
       description: "A modern business website featuring a clean layout, engaging sections, and a user-focused design that delivers a smooth browsing experience.",
@@ -13,7 +13,7 @@ const landingapi =[
     },
     {
       id:2,
-      image: '/public/finance.png',
+      image: '/finance.png',
       link:"https://finance-iota-green.vercel.app",
       tittle:"Finance",
       description: "A modern business finance website designed to help companies present their financial services with clarity, build credibility, and create a professional digital presence.",
@@ -23,7 +23,7 @@ const landingapi =[
     },
     {
       id:3,
-      image: '/public/nextcent.png',
+      image: '/nextcent.png',
       link:"https://finance-iota-green.vercel.app",
       tittle:"Nexcent",
       description: "A sleek corporate landing page that highlights business solutions, client success, and company growth through a structured layout and visually appealing design.",
@@ -33,7 +33,7 @@ const landingapi =[
     },
     {
       id:4,
-      image: '/public/wizia.png',
+      image: '/wizia.png',
       link:"https://wizia-chi.vercel.app",
       tittle:"Wizia",
       description: "A premium technology website crafted to highlight AI-powered products, business solutions, and customer engagement with a clean, modern, and conversion-focused design.",
@@ -43,7 +43,7 @@ const landingapi =[
     },
     {
       id:5,
-      image: '/public/innovate.png',
+      image: '/innovate.png',
       link:"https://salmanfarshi-dev.github.io/Innovate",
       tittle:"Innovate",
       description: "A visually engaging digital agency website that combines modern design, strategic content sections, and a refined user experience to showcase services, portfolio, and business growth with clarity and professionalism.",
@@ -53,7 +53,7 @@ const landingapi =[
     },
     {
       id:6,
-      image: '/public/kiekepotografie.png',
+      image: '/kiekepotografie.png',
       link:"https://salmanfarshi-dev.github.io/Kiekepotografie",
       tittle:"Kiekepotografie",
       description: "A beautifully designed photography portfolio that combines modern aesthetics, immersive visuals, and a polished user experience to showcase timeless moments with creativity and elegance.",
@@ -63,7 +63,7 @@ const landingapi =[
     },
     {
       id:7,
-      image: '/public/agenc.png',
+      image: '/agenc.png',
       link:"https://salmanfarshi-dev.github.io/Agency/",
       tittle:"Agenc",
       description: "A professionally designed agency website that combines modern aesthetics, strategic content sections, and a refined user experience to showcase services, business growth, and client success with clarity and impact.",
@@ -73,7 +73,7 @@ const landingapi =[
     },
     {
       id:8,
-      image: '/public/creative.png',
+      image: '/creative.png',
       link:"https://salmanfarshi-dev.github.io/Creatives/",
       tittle:"Creative",
       description: "A modern career development platform designed to connect talent with opportunities through personalized learning paths, skill development, and an engaging user experience.",
@@ -83,7 +83,7 @@ const landingapi =[
     },
     {
       id:9,
-      image: '/public/degic.png',
+      image: '/degic.png',
       link:"https://salmanfarshi-dev.github.io/Degic/",
       tittle:"Degic",
       description: "A modern startup website designed to showcase innovative ideas, creative services, and business solutions through a clean interface and engaging user experience.",
@@ -93,7 +93,7 @@ const landingapi =[
     },
     {
       id:10,
-      image: '/public/carroom.png',
+      image: '/carroom.png',
       link:"https://salmanfarshi-dev.github.io/Car-Room/",
       tittle:"Car-Room",
       description: "Crafted to provide a seamless automotive marketplace experience, combining clean visuals, intuitive navigation, and efficient vehicle search to help users discover their next car effortlessly.",
